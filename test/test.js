b156b8134b7396b770e2e51076de4e8a7ffe4a05
XhenaTegulwa <zainab.tegulwa783@gmail.com>
2023-02-19 17:24:44 +0300

    Red: Result should be 9

--- test/test.js
+++ test/test.js
@@ -6,7 +6,11 @@ describe('Testing the return values of the multiply function', () => {
         assert.equal(multiply(1, 1), 1, 'The result should be 1');
     });
 
-    it('Testing the return value of multiplying 2,3', () => {
+    it('Testing the return value of multiplying 2,2', () => {
         assert.equal(multiply(2, 2), 4, 'The result should be 4');
+    });
+
+    it('Testing the return value of multiplying 3, 3', () => {
+        assert.equal(multiply(3, 3), 4, 'The result should be 9');
     });
 });
