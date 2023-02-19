efe78d0699430bb1621799ed218d1ac21aa1f99b
XhenaTegulwa <zainab.tegulwa783@gmail.com>
2023-02-19 17:20:41 +0300

    Red: 2*2 is not 1

--- test/test.js
+++ test/test.js
@@ -5,4 +5,8 @@ describe('Testing the return values of the multiply function', () => {
     it('Testing the return value of multiplying 1,1', () => {
         assert.equal(multiply(1, 1), 1, 'The result should be 1');
     });
+
+    it('Testing the return value of multiplying 2,3', () => {
+        assert.equal(multiply(2, 2), 1, 'The result should be 4');
+    });
 });
