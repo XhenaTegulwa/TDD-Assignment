44ee274c9aa4187979d05d482cb1ce7a2cb44156
XhenaTegulwa <zainab.tegulwa783@gmail.com>
2023-02-19 17:28:56 +0300

    Green: Returns 16

--- test/test.js
+++ test/test.js
@@ -15,6 +15,6 @@ describe('Testing the return values of the multiply function', () => {
     });
 
     it('Testing the return value of multiplying 4, 4', () => {
-        assert.equal(multiply(4, 4), 9, 'The result should be 16');
+        assert.equal(multiply(4, 4), 16, 'The result should be 16');
     });
 });
